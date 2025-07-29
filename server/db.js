767255afc971737c8e39268d124d583a1984f8b3
redEevee <145461275+redEevee@users.noreply.github.com>
2025-07-29 10:13:50 +0900

    깃이그노아 정리

--- server/db.js
+++ server/db.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "akak182718@",
-  database: "board_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 connection.connect();
 module.exports = connection;
